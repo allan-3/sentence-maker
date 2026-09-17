cc5a741b3b096aa916d199b5ec493e80968672bd
Allan <allanishimwe00@gmail.com>
2026-09-17 13:21:02 +0200

    Add sentence maker project

---
+++ script.js
@@ -0,0 +1,33 @@
+// Variables for the first story
+let adjective = "tall";
+let noun = "book";
+let verb = "run";
+let place = "school";
+let adjective2 = "short";
+let noun2 = "peas";
+
+let firstStory;
+
+firstStory = "Once upon a time, there was a(n) " + adjective + " " +
+  noun + " who loved to eat " + noun2 + ". The " + noun +
+  " lived in a " + place + " and had " + adjective2 +
+  " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+// Reassign variables for the second story
+adjective = "height";
+noun = "paper";
+verb = "stop";
+place = "house";
+adjective2 = "hight";
+noun2 = "beans";
+
+let secondStory;
+
+secondStory = "Once upon a time, there was a(n) " + adjective + " " +
+  noun + " who loved to eat " + noun2 + ". The " + noun +
+  " lived in a " + place + " and had " + adjective2 +
+  " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
